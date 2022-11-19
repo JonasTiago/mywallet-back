d4f0f8fd566e4f2fd31ab039ba11029642de6d4d
Jonas Tiago <jonastiago95@gmail.com>
2022-11-18 23:39:49 -0300

    alter port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 import userRouter from "./routers/userRouters.js";
 import recordsRouter from "./routers/recordsRouters.js";
@@ -11,5 +13,5 @@ app.use(express.json());
 app.use(userRouter);
 app.use(recordsRouter);
 
-
-app.listen(5000, () => console.log("app running port:5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port:${port}`));
--- src/middlewares/userSchemaValidateMiddleware.js
+++ src/middlewares/userSchemaValidateMiddleware.js
@@ -15,8 +15,6 @@ export default function userSchemaValidate(req, res, next) {
     return res.status(422).send(errors);
   }
 
-  console.log(user)
-
   req.user = user;
 
   next();
